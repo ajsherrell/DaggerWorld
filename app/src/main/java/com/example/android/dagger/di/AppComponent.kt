6e60ed23dad1a9c021f01f86202a7a9a00cf54fd
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-27 11:51:23 -0400

    pass in context with BindsInstance

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,10 +1,21 @@
 package com.example.android.dagger.di
 
+import android.content.Context
 import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
 import dagger.Component
 
-@Component
+//definition of a Dagger component that adds info from the StorageModule to the graph.
+@Component(modules = [StorageModule::class])
 interface AppComponent {
+
+    //factory to create instances of the AppComponent
+    @Component.Factory
+    interface Factory {
+        //with @BindsInstance, the context passed in will be available in the graph
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
     //classes that can be injected by this component
     fun inject(activity: RegistrationActivity)
 }
